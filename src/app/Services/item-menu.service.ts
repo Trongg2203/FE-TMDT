a34877aee5b6f0c22e7068245d9f496f862e3e77
Trongg2203 <138137607+Trongg2203@users.noreply.github.com>
2024-06-12 14:41:26 +0700

    fix common .apiUrl

--- src/app/Services/item-menu.service.ts
+++ src/app/Services/item-menu.service.ts
@@ -1,18 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IItemMenu } from '../interfaces/itemMenu';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemMenuService {
+  apiUrl: string = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  //https://localhost:7294/api/LoaiHangHoa/getbyid?id=1
-  apiUrl = 'https://localhost:7294/api/';
-  //https://localhost:7294/api/LoaiHangHoa/getall
-  constructor(private http: HttpClient) { }
-
-  getAllLoaiHoangHoa(){
-    return this.http.get<IItemMenu[]>(this.apiUrl + 'LoaiHangHoa/getall')
+  getAllLoaiHoangHoa() {
+    return this.http.get<IItemMenu[]>(this.apiUrl + 'LoaiHangHoa/getall');
   }
 }
--- src/app/Services/list-products.service.ts
+++ src/app/Services/list-products.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IProduct, IProducts } from '../interfaces/product';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListProductsService {
-  apiUrl = 'https://localhost:7294/api/';
-  // https://localhost:7294/api/HangHoa/getall
-  // https://localhost:7294/api/HangHoa/getbyid/1
+  apiUrl:string = environment.apiUrl;
   constructor(private http:HttpClient) { }
 
   getAllProducts():Observable<IProduct[]>{

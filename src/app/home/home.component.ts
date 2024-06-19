c52c7b70aa1a332bc4deff6a042f4cc6c3571178
Trongg2203 <138137607+Trongg2203@users.noreply.github.com>
2024-06-19 10:56:21 +0700

    footer, topScroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { ListProductsComponent } from "./list-products/list-products.component";
+import { ScrollTopModule } from 'primeng/scrolltop';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [BannerComponent, ListProductsComponent]
+    imports: [BannerComponent, ListProductsComponent, ScrollTopModule]
 })
 export class HomeComponent {
 
